72e0030b9759da0c2d862b99b0f7f460a88d8910
Vaios Chrsitodoulou <vallos1501@gmail.com>
2024-08-24 15:01:16 +0300

    created structure for letters and words

---
+++ lib/game_page.dart
@@ -0,0 +1,24 @@
+// 3792
+import 'package:flutter/material.dart';
+import 'package:wordle_clone/include/classes.dart';
+
+class GamePage extends StatelessWidget {
+  List<Word> words = [];
+
+  GamePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: [
+          WordView(),
+          WordView(),
+          WordView(),
+          WordView(),
+          WordView(),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/include/classes.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+
+enum LetterStatus {
+  white,
+  grey,
+  yellow,
+  green,
+}
+
+List<Color> colors = [
+  const Color.fromARGB(255, 230, 230, 230),
+  Colors.grey,
+  const Color.fromARGB(255, 231, 215, 70),
+  Colors.green,
+];
+
+class Letter {
+  String letter = "";
+  LetterStatus status = LetterStatus.white;
+}
+
+class LetterView extends StatelessWidget {
+  final Letter letter;
+
+  const LetterView({super.key, required this.letter});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: Container(
+        width: MediaQuery.of(context).size.width / 5 - 15,
+        height: MediaQuery.of(context).size.width / 5 - 15,
+        padding: const EdgeInsets.all(8),
+        decoration: BoxDecoration(
+          color: colors[letter.status.index],
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Center(
+          child: Text(
+            letter.letter,
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 32,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Word {
+  List<Letter> letters = [];
+
+  Word({
+    required this.letters,
+  });
+}
+
+class WordView extends StatelessWidget {
+  final Word word = Word(
+    letters: [
+      Letter(),
+      Letter(),
+      Letter(),
+      Letter(),
+      Letter(),
+    ],
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children:
+          word.letters.map((letter) => LetterView(letter: letter)).toList(),
+    );
+  }
+}
